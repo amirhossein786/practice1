c3e1b7f81038ada0ea0c4563d326224291946435
amirhossein786 <amirhosseinsheikh9@gmail.com>
2024-08-20 12:38:02 +0330

    Add files via upload

---
+++ src/Word.js
@@ -0,0 +1,10 @@
+const wordList =[
+  "ace","act","add","ado","age",
+"ago","aid","aim","air","ant","any","ape","arm",
+"art","axe","ban","bar","bat","bet","bid","big",
+"bow","box","bra","bud","bum","bun","but","cab",
+"can","cap","cat","cob","cop","cow","coy","cry",
+"cut","dab","dam","die","dig","dug","dim","dog",
+"dry","din","dot","dub","due"
+];
+  export default wordList;
